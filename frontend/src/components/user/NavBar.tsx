c14dfae29d343ba5d647b15a4643c57e5890351e
rahmatdeep <rahmatdeep@gmail.com>
2024-04-03 02:52:21 +0530

    fix Navbar

--- frontend/src/components/user/NavBar.tsx
+++ frontend/src/components/user/NavBar.tsx
@@ -4,11 +4,13 @@ import { ModeToggle } from "./ModeToggle";
 
 export function NavBar() {
   return (
-    <header className="container sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className=" flex h-14 max-w-screen-2xl items-center ">
-        <MainNav />
-        <MobileNav />
-        <ModeToggle />
+    <header className=" sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container">
+        <div className=" flex h-14 max-w-screen-2xl items-center ">
+          <MainNav />
+          <MobileNav />
+          <ModeToggle />
+        </div>
       </div>
     </header>
   );
